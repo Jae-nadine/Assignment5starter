88f9fab042a2f0994c797d8872a47d66f152eb78
Jae-Nadine <jae.nadine@icloud.com>
2024-08-11 05:28:55 -0700

    Assignment5
    Initial Commit

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,24 +1,49 @@
 package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
-	Object[] items = new Object[10];
+	private Object[] items = new Object[10];
+	private int size = 0;
+	
+	public CustomArrayList() {
+		
+	}
 
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
-		return false;
+		if (size == items.length) {
+			growArray();
 	}
+		items[size++] = item;
+		return true;
+	}
+	
+
+	private void growArray() {
+		Object [] newArray = new Object[items.length * 2];
+		System.arraycopy(items, 0, newArray, 0, items.length);
+		items = newArray;
+	}
+	
+	@Override
+	public T get(int index) {
+		if (index >= size || index < 0) {
+			throw new IndexOutOfBoundsException("Index out of bounds");
+		}
+		return (T) items[index];
+	}
+	
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public T get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return size;
 	}
 	
 }
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/coderscampus/arraylist/CustomListApplication.java
@@ -0,0 +1,17 @@
+package com.coderscampus.arraylist;
+
+public class CustomListApplication {
+	
+	public static void main(String[] args) {
+		
+		CustomList<String> myCustomList = new CustomArrayList<>();
+			for (int i = 1; i <= 25; i++) {
+				myCustomList.add("element " + i);
+			}
+			
+			for (int i = 0; i < myCustomList.getSize(); i++) {
+				System.out.println(myCustomList.get(i));
+			}
+	}
+
+}
